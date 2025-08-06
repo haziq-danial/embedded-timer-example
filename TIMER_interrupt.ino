57ddb9b1e1c64bea72d287309ede9b41872ca1b3
haziq danial <haziqdanial031@gmail.com>
2025-08-06 10:56:04 +0800

    fixed gpio read issue

--- TIMER_interrupt.ino
+++ TIMER_interrupt.ino
@@ -2,16 +2,18 @@
 
 int main(void) {
   volatile uint32_t delay;
-  volatile uint64_t timeout;
+  volatile uint64_t LED_timeout, sw_timeout;
   uint8_t toggle = 0;
+  uint8_t sw;
 
   SYS_Error_Check(SYS_Init());
 
-  timeout = SYS_TICK + 1000;
+  LED_timeout = SYS_TICK + 1000;
+  sw_timeout = SYS_TICK + 20;
 
   for (;;) {
-    if (SYS_TICK > timeout) {
-      timeout = SYS_TICK + 1000;
+    if (SYS_TICK > LED_timeout) {
+      LED_timeout = SYS_TICK + 1000;
 
       if (toggle) {
         SYS_Error_Check(GPIO_Write(LED_DEBUG_PIN, GPIO_LOW));
@@ -20,6 +22,19 @@ int main(void) {
         SYS_Error_Check(GPIO_Write(LED_DEBUG_PIN, GPIO_HIGH));
         toggle = 1;
       }
+
     }
+
+    // this currently doesn't work
+    if (SYS_TICK > sw_timeout) {
+      sw_timeout = SYS_TICK + 20;
+
+      SYS_Error_Check(GPIO_Read(SW1_PIN, &sw));
+
+      if(sw == GPIO_HIGH) SYS_Error_Check(GPIO_Write(LED_GREEN_PIN, GPIO_HIGH));
+      else SYS_Error_Check(GPIO_Write(LED_GREEN_PIN, GPIO_LOW));
+
+    }
+
   }
 }
